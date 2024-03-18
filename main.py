d8dec2aa02fdb01fed744844557ff37d6b31a12d
Navneet Singh <singhnavneet@salesforce.com>
2024-03-18 12:42:54 +0530

    initial setup

---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+import sys
+import ansible_runner
+
+out, err, rc = ansible_runner.run_command(
+    executable_cmd='ansible-playbook',
+    cmdline_args=['/home/singhnavneet.su/ansible_setup/project/device_updates.yaml',
+                  '-i', 'inventory', '-vvvv', '-k'],
+    input_fd=sys.stdin,
+    output_fd=sys.stdout,
+    error_fd=sys.stderr,
+)
+print("rc: {}".format(out))
+print("out: {}".format(err))
+print("err: {}".format(rc))
