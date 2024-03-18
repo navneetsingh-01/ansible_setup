f4ba979b1efec2d53611577c264e906a02278ab4
Navneet Singh <singhnavneet@salesforce.com>
2024-03-18 13:31:54 +0530

    some changes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import ansible_runner
 out, err, rc = ansible_runner.run_command(
     executable_cmd='ansible-playbook',
     cmdline_args=['/home/singhnavneet.su/ansible_setup/project/device_updates.yaml',
-                  '-i', 'inventory', '-vvvv', '--vault-password-file', '/home/singhnavneet.su/ansible_setup/vault_password'],
+                  '-i', 'inventory', '-vvvv', '--vault-password-file', 'vault_password'],
     input_fd=sys.stdin,
     output_fd=sys.stdout,
     error_fd=sys.stderr,
