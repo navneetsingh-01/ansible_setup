53572fbb987e1745493d84ac32993461814beeaa
Navneet Singh <singhnavneet@salesforce.com>
2024-03-18 13:53:17 +0530

    some changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import ansible_runner
 
 out, err, rc = ansible_runner.run_command(
     executable_cmd='ansible-playbook',
-    cmdline_args=['/home/singhnavneet.su/ansible_setup/project/device_updates.yaml',
+    cmdline_args=['/home/singhnavneet.su/ansible_setup/project/validate_algorithm.yaml',
                   '-i', 'inventory', '-vvvv', '--vault-password-file', 'vault_password'],
     input_fd=sys.stdin,
     output_fd=sys.stdout,
