36ea6e7e3da0d25b464fd844c8d287014ed898e5
Navneet Singh <singhnavneet@salesforce.com>
2024-05-23 11:22:26 +0530

    some change

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ import time
 import sys
 import ansible_runner
 
+# out, err, rc = ansible_runner.run_command(
+#     executable_cmd='ansible-playbook',
+#     cmdline_args=['/home/singhnavneet.su/ansible_setup/project/validate_algorithm.yaml',
+#                   '-i', 'inventory', '-vvvv', '--vault-password-file', 'vault_password'],
+#     input_fd=sys.stdin,
+#     output_fd=sys.stdout,
+#     error_fd=sys.stderr,
+# )
+# print("rc: {}".format(out))
+# print("out: {}".format(err))
+# print("err: {}".format(rc))
+
 out, err, rc = ansible_runner.run_command(
     executable_cmd='ansible-playbook',
-    cmdline_args=['/home/singhnavneet.su/ansible_setup/project/validate_algorithm.yaml',
+    cmdline_args=['/home/singhnavneet.su/ansible_setup/project/run_commands.yaml',
                   '-i', 'inventory', '-vvvv', '--vault-password-file', 'vault_password'],
     input_fd=sys.stdin,
     output_fd=sys.stdout,
